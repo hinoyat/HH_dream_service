2bc27dc7c11fa8b1a7a78e9d174dee975993a118
Hinoyat <hhwj2270@gmail.com>
2025-03-25 22:09:01 +0900

    Feat: 좌표 고도화

---
+++ ver0.1/diary-service/src/main/java/com/c202/diary/diary/model/request/DiaryUpdateRequestDto.java
@@ -0,0 +1,21 @@
+package com.c202.diary.diary.model.request;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class DiaryUpdateRequestDto {
+
+    private String title;
+
+    private String content;
+
+    private String dreamDate;
+
+    private List<String> tags;
+
+    private String mainEmotion;
+}
