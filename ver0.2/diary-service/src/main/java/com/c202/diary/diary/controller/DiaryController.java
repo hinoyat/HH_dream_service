f03773c20e0e1d4ad9e5f571646d9a3657b8f229
Hinoyat <hhwj2270@gmail.com>
2025-03-26 21:09:15 +0900

    Feat: S3 추가

---
+++ ver0.2/diary-service/src/main/java/com/c202/diary/diary/controller/DiaryController.java
@@ -0,0 +1,92 @@
+package com.c202.diary.diary.controller;
+
+import com.c202.diary.diary.model.response.UniverseDataResponseDto;
+import com.c202.diary.tag.service.TagService;
+import com.c202.dto.ResponseDto;
+import com.c202.diary.diary.model.request.DiaryCreateRequestDto;
+import com.c202.diary.diary.model.request.DiaryUpdateRequestDto;
+import com.c202.diary.diary.model.response.DiaryDetailResponseDto;
+import com.c202.diary.diary.model.response.DiaryListResponseDto;
+import com.c202.diary.diary.service.DiaryService;
+import com.c202.exception.CustomException;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/api/diaries")
+@RequiredArgsConstructor
+@Validated
+public class DiaryController {
+
+    private final DiaryService diaryService;
+    private final TagService tagService;
+
+    @PostMapping("")
+    public ResponseEntity<ResponseDto<DiaryDetailResponseDto>> createDiary(
+            @RequestHeader("X-User-Seq") @NotNull Integer userSeq,
+            @RequestBody DiaryCreateRequestDto dto
+    ) {
+        return ResponseEntity.ok(ResponseDto.success(201, "일기 작성 완료", diaryService.createDiary(userSeq, dto)));
+    }
+
+    @PutMapping("/{diarySeq}")
+    public ResponseEntity<ResponseDto<DiaryDetailResponseDto>> updateDiary(
+            @RequestHeader("X-User-Seq") @NotNull Integer userSeq,
+            @RequestBody DiaryUpdateRequestDto dto,
+            @PathVariable Integer diarySeq
+    ) {
+        return ResponseEntity.ok(ResponseDto.success(200, "일기 수정 완료", diaryService.updateDiary(diarySeq, userSeq, dto)));
+    }
+
+    @DeleteMapping("/{diarySeq}")
+    public ResponseEntity<ResponseDto<Objects>> deleteDiary(
+            @RequestHeader("X-User-Seq") @NotNull Integer userSeq,
+            @PathVariable Integer diarySeq
+    ) {
+        diaryService.deleteDiary(diarySeq, userSeq);
+        return ResponseEntity.ok(ResponseDto.success(204, "일기 삭제 완료"));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<ResponseDto<List<DiaryListResponseDto>>> getMyDiaries(
+            @RequestHeader("X-User-Seq") @NotNull Integer userSeq
+    ) {
+        return ResponseEntity.ok(ResponseDto.success(200, "내 일기 조회 완료", diaryService.getMyDiaries(userSeq)));
+    }
+
+    @GetMapping("/users/{userSeq}")
+    public ResponseEntity<ResponseDto<List<DiaryListResponseDto>>> getUserDiaries(
+            @PathVariable Integer userSeq
+    ) {
+        return ResponseEntity.ok(ResponseDto.success(200, "사용자 일기 조회 완료", diaryService.getUserDiaries(userSeq)));
+    }
+
+    @GetMapping("/{diarySeq}")
+    public ResponseEntity<ResponseDto<DiaryDetailResponseDto>> getDiary(
+            @PathVariable Integer diarySeq
+    ) {
+        return ResponseEntity.ok(ResponseDto.success(200, "일기 상세 조회 완료", diaryService.getDiary(diarySeq)));
+    }
+
+    @PutMapping("/{diarySeq}/visibility")
+    public ResponseEntity<ResponseDto<DiaryDetailResponseDto>> toggleDiaryIsPublic(
+            @RequestHeader("X-User-Seq") @NotNull Integer userSeq,
+            @PathVariable Integer diarySeq
+    ) {
+        return ResponseEntity.ok(ResponseDto.success(200, "일기 공개 상태 변경 완료", diaryService.toggleDiaryIsPublic(diarySeq, userSeq)));
+    }
+
+    @GetMapping("/universe")
+    public ResponseEntity<ResponseDto<UniverseDataResponseDto>> getUniverseData(
+            @RequestHeader("X-User-Seq") @NotNull Integer userSeq
+    ) {
+        return ResponseEntity.ok(ResponseDto.success(200, "우주 데이터 조회 완료", diaryService.getUniverseData(userSeq)));
+    }
+
+}
---
+++ ver0.2/diary-service/src/main/java/com/c202/diary/tag/entity/DiaryTag.java
@@ -0,0 +1,33 @@
+package com.c202.diary.tag.entity;
+
+import com.c202.diary.diary.entity.Diary;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DiaryTag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer diaryTagSeq;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)  // Diary와 매핑
+    @JoinColumn(name = "diarySeq")
+    private Diary diary;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // Tag와 매핑
+    @JoinColumn(name = "tagSeq")
+    private Tag tag;
+
+    @Column(nullable = false, length = 15)
+    private String createdAt;
+
+    @Column(nullable = false, length = 15)
+    private String updatedAt;
+
+
+}
